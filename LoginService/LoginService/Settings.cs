128740c0167979d3c6c574e3ed9b939b8b3e2ba9
Elizabeth Halper <elhalper@microsoft.com>
2018-04-03 14:13:41 -0700

    added Settings class

---
+++ LoginService/LoginService/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LoginService
+{
+    
+    internal class Settings
+    {
+        public static string ConnectionString = Environment.GetEnvironmentVariable("ConnectionString"); // , EnvironmentVariableTarget.Process);
+        public static string ApplicationInsightsKey = Environment.GetEnvironmentVariable("InstrumentationKey");
+
+        public Settings()
+        {
+        }
+    }
+}
